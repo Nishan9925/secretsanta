66c26b2fe6de778c85d184904c706aefffe72c82
nshan_zakaryan <zakaryannshan@gmail.com>
2024-12-24 15:59:51 +0400

    Commented few unused lines of code, will be used in the future for better UI

--- src/components/PlayersContainer.tsx
+++ src/components/PlayersContainer.tsx
@@ -1,36 +1,37 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState, store } from "../store";
 import { generatePlayers } from "../features/randomSlice";
-import { Table } from "antd";
+// import { Table } from "antd";
+
 function PlayersContainer() {
   const { players, newArray } = useSelector((store: RootState) => store.players);
   const dispatch: AppDispatch = useDispatch();
 
-  const columnSanta = [
-    {
-      title: "Santa",
-      dataIndex: "santa",
-      key: "santa",
-    }
-  ]
+//   const columnSanta = [
+//     {
+//       title: "Santa",
+//       dataIndex: "santa",
+//       key: "santa",
+//     }
+//   ]
 
-  const staticNames = players.map((player) => ({
-    ...player,
-    key: player.id,
-  }));
+//   const staticNames = players.map((player) => ({
+//     ...player,
+//     key: player.id,
+//   }));
 
   return (
     <div style={{ display: "flex",  }}>
       <h1>Players</h1>
-      {/* <div style={{ display: "flex", flexFlow: "column" }}>
+      <div style={{ display: "flex", flexFlow: "column" }}>
         {players.map((player) => (
           <div key={player.id}>
             <p>{player.name}</p>
           </div>
         ))}
-      </div> */}
+      </div>
       <div>
-        <Table columns={columnSanta} dataSource={staticNames}/>
+        {/* <Table columns={columnSanta} dataSource={staticNames}/> */}
       </div>
       <div style={{ display: "flex", flexFlow: "column", gap: ".01rem" }}>
         {Object.entries(newArray).map(([playerId, playerName]) => (
